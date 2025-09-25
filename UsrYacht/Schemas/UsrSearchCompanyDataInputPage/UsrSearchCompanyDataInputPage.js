497e07f2a857e4d56eeb84afff84246978e7fd8e
gamoradima <gamoradima@gmail.com>
2025-09-25 16:45:26 +0300

    Call Web Service process item example

---
+++ UsrYacht/Schemas/UsrSearchCompanyDataInputPage/UsrSearchCompanyDataInputPage.js
@@ -0,0 +1,78 @@
+define("UsrSearchCompanyDataInputPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "Input_24iev2p",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.PageParameters_UsrTextParameter1_aa38fjk",
+					"labelPosition": "above",
+					"control": "$PageParameters_UsrTextParameter1_aa38fjk",
+					"multiline": false
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Input_8plmo0d",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 2,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.PageParameters_UsrTextParameter2_lr8kcpi",
+					"labelPosition": "above",
+					"control": "$PageParameters_UsrTextParameter2_lr8kcpi",
+					"multiline": false
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 1
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes"
+				],
+				"values": {
+					"PageParameters_UsrTextParameter1_aa38fjk": {
+						"modelConfig": {
+							"path": "PageParameters.UsrTextParameter1"
+						}
+					},
+					"PageParameters_UsrTextParameter2_lr8kcpi": {
+						"modelConfig": {
+							"path": "PageParameters.UsrTextParameter2"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [],
+				"values": {
+					"dataSources": {}
+				}
+			}
+		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
